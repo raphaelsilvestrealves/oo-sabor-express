e2aabba1c9097fa3ba783a0378c5b28f8f9630d2
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-04-17 17:49:03 -0300

    Refactor Restaurante class to use private attributes and improve listing format

--- models/restaurante.py
+++ models/restaurante.py
@@ -2,22 +2,30 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self.categoria}'
 
-    def listar_restaurantes():
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Ativo'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
+    @property
+    def ativo(self):
+        return '☑' if self._ativo else '☐'
     
-restaurante_praca = Restaurante(nome='Praça', categoria='Gourmet')
-restaurante_pizza = Restaurante(nome='Pizza Express', categoria='Italiana')
+    #metodo para os objetos
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-
+restaurante_praca = Restaurante(nome='praça', categoria='Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante(nome='pizza Express', categoria='Italiana')
 
 Restaurante.listar_restaurantes()
