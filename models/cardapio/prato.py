aac43b19f39d465d6f44fc7810f9b13628cf3ff7
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-04-28 16:37:38 -0300

    add: create Bebida and Prato classes extending ItemCardapio

---
+++ models/cardapio/prato.py
@@ -0,0 +1,6 @@
+from models.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio):
+    def __init__(self, nome, preco, descricao):
+        super().__init__(nome, preco)
+        self.descricao = descricao
