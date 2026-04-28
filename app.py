739f5ae8c411e911f9aeff4111db0419bcd9153c
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-04-28 17:15:59 -0300

    add: implement aplicar_desconto method in Bebida and Prato classes

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ from models.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melância', 5.0, 'grande')
+bebida_suco.aplicar_desconto()
 prato_pao = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
+prato_pao.aplicar_desconto()
 restaurante_praca.adicionar_no_cardapio(bebida_suco)
 restaurante_praca.adicionar_no_cardapio(prato_pao)
 
 def main():
     restaurante_praca.exibir_cardapio
+    
 if __name__ == '__main__':
     main()
