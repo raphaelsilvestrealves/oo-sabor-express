9122bca2b4cb81548e33dfe8c325e3dbe4f20767
Raphael Silvestre Alves <ssevlaa@gmail.com>
2026-04-28 17:00:55 -0300

    add: implement cardapio management in Restaurante and update Bebida and Prato classes

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from models.restaurante import Restaurante
+from models.cardapio.bebida import Bebida
+from models.cardapio.prato import Prato
+
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Laís', 8)
-restaurante_praca.receber_avaliacao('Emy', 2)
-
+bebida_suco = Bebida('Suco de Melância', 5.0, 'grande')
+prato_pao = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_pao)
 
 def main():
-    Restaurante.listar_restaurantes()
-    pass
-
+    restaurante_praca.exibir_cardapio
 if __name__ == '__main__':
     main()
